c57e678d997912649133f9099a41d9fb44ac5a7f
AlexAtSAE <120990uk@saeinstitute.edu>
2026-03-31 08:30:10 +0100

    sprit3e

---
+++ Assets/Notes.cs
@@ -0,0 +1,16 @@
+#region intro
+/* THIS CS FILE IS JUST FOR NOTES
+ * 
+ */
+#endregion
+
+
+#region Fonts
+/* Good font is "Electrolize"
+ * https://www.photopea.com
+ * Right click the layer with text, blending options
+ * 
+ * 
+ */
+
+#endregion
